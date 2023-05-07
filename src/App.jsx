345007f5d2d2a5920c3e06f738c79950ddfaca9a
mrduman <omer.duman@gmx.de>
2023-05-07 16:29:17 +0200

    watch page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+
 import "./app.scss"
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
 
-
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-}
+const App = () => {
+  return <Home />;
+};
 
 export default App;
