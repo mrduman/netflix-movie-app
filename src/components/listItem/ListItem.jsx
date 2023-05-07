a75fd6acc98bc9fca746dda25dbe66fa5825bb6f
mrduman <omer.duman@gmx.de>
2023-05-07 16:53:39 +0200

    img changed

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -14,7 +14,8 @@ function ListItem({ index }) {
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
-            <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+                alt="" />
             {isHovered && (
                 <>
                     <video src={trailer} autoPlay={true}></video>
